790328bfdc467a87210504423dd4cbe3e16bb3e3
martin nielsen fig <martin.fig@hotmail.com>
2017-03-18 19:14:08 +0100

    Changed discountcodedto to reflex xenas

--- src/Xena.Contracts/Helpers/DiscountCodeDto.cs
+++ src/Xena.Contracts/Helpers/DiscountCodeDto.cs
@@ -2,16 +2,16 @@
 {
     public class DiscountCodeDto
     {
-        public long Version { get; set; }
+        public long? Version { get; set; }
         public bool IsDeactivated { get; set; }
-        public long FiscalSetupId { get; set; }
+        public long? FiscalSetupId { get; set; }
         public string CreatedAt { get; set; }
-        public long CreatedBy { get; set; }
+        public long? CreatedBy { get; set; }
         public string Description { get; set; }
         public string Code { get; set; }
         public string ExpirationDateDays { get; set; }
-        public long ReferenceFiscalSetupId { get; set; }
+        public long? ReferenceFiscalSetupId { get; set; }
         public string ReferenceFiscalSetupName { get; set; }
-        public long Id { get; set; }
+        public long? Id { get; set; }
     }
 }
