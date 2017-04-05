67502154370df242b5f9053331be1bf7ab03677b
DESKTOP-EFUI98M\methg <methgaard@gmail.com>
2017-04-05 13:21:10 +0200

    finishing up

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -10,5 +10,5 @@ dotnet pack ./src/Xena.Contracts/Xena.Contracts.csproj --configuration debug -o 
   # pushes the pack to nuget.org 
   dotnet nuget push /home/travis/build/EG-Xena/Xena.Contracts/src/Xena.Contracts/nugetPackage/*.nupkg -k "$XENA_CONTRACTS_NUGET_KEY" -s https://www.nuget.org/api/v2/package
 } || {
-  echo -e "\e[31mRed An error occured trying to push the package to nuget. Please check that you have updated the version number." 
+  echo -e "\e[31m An error occured trying to push the package to nuget. Please check that you have updated the version number." 
 }
