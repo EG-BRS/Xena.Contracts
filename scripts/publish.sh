0236843d1dcc50c3ea8afdace73d14f9fe0ecbb4
DESKTOP-EFUI98M\methg <methgaard@gmail.com>
2017-04-05 12:12:52 +0200

    changed path (again) to .nupkg in nuget pack command in publish.sh

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,7 +4,7 @@
 set -e
 
 dotnet pack ./src/Xena.Contracts/Xena.Contracts.csproj --configuration debug -o nugetPackage
-dotnet nuget push /src/Xena.Contracts/nugetPackage/*.nupkg -k 75066627-6f3d-47f6-bd90-b165f912392e -s https://www.nuget.org/api/v2/package
+dotnet nuget push /home/travis/build/EG-Xena/Xena.Contracts/src/Xena.Contracts/nugetPackage/*.nupkg -k 75066627-6f3d-47f6-bd90-b165f912392e -s https://www.nuget.org/api/v2/package
 
 
 # Anders '  test key 75066627-6f3d-47f6-bd90-b165f912392e'
