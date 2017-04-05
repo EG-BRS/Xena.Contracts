26e5ba070c3e6e13f832943146342a460912a021
DESKTOP-EFUI98M\methg <methgaard@gmail.com>
2017-04-05 11:11:21 +0200

    travis now enabled for repo - trying again

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -9,3 +9,4 @@ dotnet nuget push nugetPackage/*.nupkg -k 75066627-6f3d-47f6-bd90-b165f912392e
 # Anders '  test key 75066627-6f3d-47f6-bd90-b165f912392e'
 
 # husted Xena.Contracts key 086f2040-5566-42ec-857e-2d8753910863
+ 
