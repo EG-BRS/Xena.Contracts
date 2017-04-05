44792389ce33931cb684defb095aefc345fe572d
DESKTOP-EFUI98M\methg <methgaard@gmail.com>
2017-04-05 13:07:52 +0200

    added error message in case of error in pushing package to nuget

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -8,7 +8,7 @@ dotnet pack ./src/Xena.Contracts/Xena.Contracts.csproj --configuration debug -o 
 
 {
   # pushes the pack to nuget.org 
-  dotnet nuget push /home/travis/build/EG-Xena/Xena.Contracts/src/Xena.Contracts/nugetPackage/*.nupkg -k "$XENA_CONTRACTS_NUGET_KEY" -s https://www.nuget.org/api/v2/package &&
+  dotnet nuget push /home/travis/build/EG-Xena/Xena.Contracts/src/Xena.Contracts/nugetPackage/*.nupkg -k "$XENA_CONTRACTS_NUGET_KEY" -s https://www.nuget.org/api/v2/package
 } || {
   echo An error occured trying to push the package to nuget. Please check that you have updated the version number. 
 }
