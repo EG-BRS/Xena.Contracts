b20574dc62669111706e9bd14c874d99ba7de592
soerenschmidt89 <sosch@eg.dk>
2017-05-10 11:22:07 +0200

    GAB-399 set everything in OrderTaskLineDto to nullable

--- src/Xena.Contracts/Domain/OrderTaskLineDto.cs
+++ src/Xena.Contracts/Domain/OrderTaskLineDto.cs
@@ -6,44 +6,44 @@ namespace Xena.Contracts.Domain
 {
     public class OrderTaskLineDto
     {
-        public int Version { get; set; }
-        public bool IsDeactivated { get; set; }
-        public int FiscalSetupId { get; set; }
+        public int? Version { get; set; }
+        public bool? IsDeactivated { get; set; }
+        public int? FiscalSetupId { get; set; }
         public string CreatedAt { get; set; }
-        public int CreatedBy { get; set; }
-        public int FiscalDateDays { get; set; }
-        public int CostTypeId { get; set; }
+        public int? CreatedBy { get; set; }
+        public int? FiscalDateDays { get; set; }
+        public int? CostTypeId { get; set; }
         public string Description { get; set; }
-        public double Amount { get; set; }
-        public double Quantity { get; set; }
+        public double? Amount { get; set; }
+        public double? Quantity { get; set; }
         public string CurrencyAbbreviation { get; set; }
-        public int ArticleId { get; set; }
-        public int ActivityTypeId { get; set; }
-        public int ArticleVariantId { get; set; }
-        public int UnitId { get; set; }
-        public int LocationId { get; set; }
-        public int OrderTaskId { get; set; }
-        public int ResourceId { get; set; }
-        public int ResponsibleId { get; set; }
+        public int? ArticleId { get; set; }
+        public int? ActivityTypeId { get; set; }
+        public int? ArticleVariantId { get; set; }
+        public int? UnitId { get; set; }
+        public int? LocationId { get; set; }
+        public int? OrderTaskId { get; set; }
+        public int? ResourceId { get; set; }
+        public int? ResponsibleId { get; set; }
         public string ResponsibleName { get; set; }
         public string ResourceName { get; set; }
         public string CostTypeDescription { get; set; }
         public string ArticleNumber { get; set; }
         public string ArticleDescription { get; set; }
-        public bool ArticleHasInventoryManagement { get; set; }
-        public bool ArticleHasVariants { get; set; }
+        public bool? ArticleHasInventoryManagement { get; set; }
+        public bool? ArticleHasVariants { get; set; }
         public string ArticleVariantAbbreviation { get; set; }
         public string ActivityTypeAbbreviation { get; set; }
         public string ActivityTypeDescription { get; set; }
         public string UnitAbbreviation { get; set; }
         public string LocationAbbreviation { get; set; }
         public string OrderTaskDescription { get; set; }
-        public int OrderTaskIndex { get; set; }
-        public int OrderNumber { get; set; }
-        public int DocumentCount { get; set; }
+        public int? OrderTaskIndex { get; set; }
+        public int? OrderNumber { get; set; }
+        public int? DocumentCount { get; set; }
         public string OrderTaskAbbreviation { get; set; }
         public string FiscalDateDaysFriendly { get; set; }
         public string ArticleAbbreviation { get; set; }
-        public int Id { get; set; }
+        public int? Id { get; set; }
     }
 }
