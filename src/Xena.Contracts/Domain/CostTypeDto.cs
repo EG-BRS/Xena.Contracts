eb9e67fb1a72aa1bbad666720dcddf7e462ce328
EGDK\xxufh <xxufh@eg.dk>
2017-07-27 13:08:13 +0200

    Added CostTypeDto

---
+++ src/Xena.Contracts/Domain/CostTypeDto.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace Xena.Contracts.Domain
+{
+    public class CostTypeDto : EntityDto
+    {
+        public long? Id { get; set; }
+        public int Version { get; set; }
+        public bool IsDeactivated { get; set; }
+        public long FiscalSetupId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Description { get; set; }
+        public long DefaultMargin { get; set; }
+        public long DefaultArticleGroupId { get; set; }
+        public string DefaultArticleGroupDescription { get; set; }
+    }
+}
