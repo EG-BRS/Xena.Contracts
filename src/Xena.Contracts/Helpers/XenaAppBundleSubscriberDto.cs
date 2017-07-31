96e670e00c0192dc09857e8a260329758167a132
soerenschmidt89 <sosch@eg.dk>
2017-07-31 11:50:32 +0200

    GAB-590 added xenaAppBundleSubscriberDto

---
+++ src/Xena.Contracts/Helpers/XenaAppBundleSubscriberDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xena.Contracts.Domain;
+
+namespace Xena.Contracts.Helpers
+{
+    public class XenaAppBundleSubscriberDto : FiscalDto, IHasIdDto
+    {
+        public long? Id { get; set; }
+        public long XenaAppBundleId { get; set; }
+        public long? UserId { get; set; }
+        public long ReferenceFiscalSetupId { get; set; }
+        public long PartnerResourceContextId { get; set; }
+        public string ResourceName { get; set; }
+        public string ResourceSenderEmail { get; set; }
+        public string ReferenceFiscalSetupName { get; set; }
+        public int? ExpiresOnDays { get; set; }
+    }
+}
