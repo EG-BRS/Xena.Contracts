ca0717ab066a9088344b6d64643dac1ce7a2a169
EGDK\xxufh <xxufh@eg.dk>
2017-08-13 16:08:09 +0200

    Added ResourcePostDto

---
+++ src/Xena.Contracts/Domain/ResourcePostDto.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Xena.Contracts.Domain
+{
+    public class ResourcePostDto : EntityDto
+    {
+        public long? OrderLineId { get; set; }
+        public string Type { get; set; }
+        public long? OrderTaskId { get; set; }
+        public int? OrderTaskIndex { get; set; }
+        public string OrderTaskDescription { get; set; }
+        public string Description { get; set; }
+        public int? OrderNumber { get; set; }
+        public string OrderName { get; set; }
+        public int? CostTypeId { get; set; }
+        public long? ProjectId { get; set; }
+        public long? UnitId { get; set; }
+        public string UnitAbbreviation { get; set; }
+        public string CostTypeDescription { get; set; }
+        public decimal? Quantity { get; set; }
+        public decimal? CostEach { get; set; }
+        public decimal? Total { get; set; }
+        public decimal? OrderLineWeight { get; set; }
+        public long? ArticleId { get; set; }
+        public long? ResourceId { get; set; }
+        public string ResourceName { get; set; }
+        public long? ActivityTypeId { get; set; }
+        public long? OrderId { get; set; }
+        public bool IsApproved { get; set; }
+        public string ArticleNumber { get; set; }
+        public string ArticleDescription { get; set; }
+        public string ActivityTypeDescription { get; set; }
+        public string ActivityTypeAbbreviation { get; set; }
+        public bool ActivityTypeIsBillable { get; set; }
+        public bool ActivityTypeIsAtWork { get; set; }
+        public bool ActivityTypeIsPaid { get; set; }
+        public int? OrderLineUnitId { get; set; }
+        public string OrderLineUnitAbbreviation { get; set; }
+        public int? OrderLineArticleId { get; set; }
+        public int? OrderLineArticleGroupId { get; set; }
+        public string OrderLineArticleGroupDescription { get; set; }
+        public string OrderLineArticleNumber { get; set; }
+        public int? OrderLineQuantity { get; set; }
+        public string OrderTaskAbbreviation { get; set; }
+        public long? ResponsibleId { get; set; }
+        public string ResponsibleName { get; set; }
+        public int? StartDateDays { get; set; }
+        public int? StartTimeMinutes { get; set; }
+        public int? StartTimeHours { get; set; }
+        public int? DurationHours { get; set; }
+        public int? DurationMinutes { get; set; }
+        public decimal? Duration { get; set; }
+        public string StartFriendly { get; set; }
+        public string StartTimeFriendly { get; set; }
+        public int? EndTimeHours { get; set; }
+        public int? EndTimeMinutes { get; set; }
+        public string EndTimeFriendly { get; set; }
+    }
+}
