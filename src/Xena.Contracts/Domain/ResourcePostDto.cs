93d79c74119cb6f8eb27c44d515ca78c8c7b55cd
MikeMarius <mike.hansen@eg.dk>
2017-08-30 14:54:10 +0200

    Changed ResourcePostDt to match Xena definition -> Ver. 1.0.37

--- src/Xena.Contracts/Domain/ResourcePostDto.cs
+++ src/Xena.Contracts/Domain/ResourcePostDto.cs
@@ -7,22 +7,22 @@ namespace Xena.Contracts.Domain
         public long? OrderLineId { get; set; }
         public string Type { get; set; }
         public long? OrderTaskId { get; set; }
-        public int? OrderTaskIndex { get; set; }
+        public long? OrderTaskIndex { get; set; }
         public string OrderTaskDescription { get; set; }
         public string Description { get; set; }
         public int? OrderNumber { get; set; }
         public string OrderName { get; set; }
-        public int? CostTypeId { get; set; }
+        public long? CostTypeId { get; set; }
         public long? ProjectId { get; set; }
         public long? UnitId { get; set; }
         public string UnitAbbreviation { get; set; }
         public string CostTypeDescription { get; set; }
         public decimal? Quantity { get; set; }
-        public decimal? CostEach { get; set; }
-        public decimal? Total { get; set; }
+        public decimal CostEach { get; set; }
+        public decimal Total { get; set; }
         public decimal? OrderLineWeight { get; set; }
         public long? ArticleId { get; set; }
-        public long? ResourceId { get; set; }
+        public long ResourceId { get; set; }
         public string ResourceName { get; set; }
         public long? ActivityTypeId { get; set; }
         public long? OrderId { get; set; }
@@ -31,20 +31,20 @@ namespace Xena.Contracts.Domain
         public string ArticleDescription { get; set; }
         public string ActivityTypeDescription { get; set; }
         public string ActivityTypeAbbreviation { get; set; }
-        public bool ActivityTypeIsBillable { get; set; }
-        public bool ActivityTypeIsAtWork { get; set; }
-        public bool ActivityTypeIsPaid { get; set; }
-        public int? OrderLineUnitId { get; set; }
+        public bool? ActivityTypeIsBillable { get; set; }
+        public bool? ActivityTypeIsAtWork { get; set; }
+        public bool? ActivityTypeIsPaid { get; set; }
+        public long? OrderLineUnitId { get; set; }
         public string OrderLineUnitAbbreviation { get; set; }
-        public int? OrderLineArticleId { get; set; }
-        public int? OrderLineArticleGroupId { get; set; }
+        public long? OrderLineArticleId { get; set; }
+        public long? OrderLineArticleGroupId { get; set; }
         public string OrderLineArticleGroupDescription { get; set; }
         public string OrderLineArticleNumber { get; set; }
-        public int? OrderLineQuantity { get; set; }
+        public decimal? OrderLineQuantity { get; set; }
         public string OrderTaskAbbreviation { get; set; }
         public long? ResponsibleId { get; set; }
         public string ResponsibleName { get; set; }
-        public int? StartDateDays { get; set; }
+        public int StartDateDays { get; set; }
         public int? StartTimeMinutes { get; set; }
         public int? StartTimeHours { get; set; }
         public int? DurationHours { get; set; }
