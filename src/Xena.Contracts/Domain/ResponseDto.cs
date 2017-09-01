60c6ab1fd32609ea93b7b94e836ab8c387b10248
EGDK\xxufh <xxufh@eg.dk>
2017-09-01 12:55:38 +0200

    Added a response dto -> version 1.0.38

---
+++ src/Xena.Contracts/Domain/ResponseDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Xena.Contracts.Domain
+{
+    public class ResponseDto
+    {
+        public long Id { get; set; }
+        public List<string> Messages { get; set; }
+        public string NewState { get; set; }
+        public bool Success { get; set; }
+    }
+}
