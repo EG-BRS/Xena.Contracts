e8c48f88e435e75bc247dbcfbb0ff7bf7f64580e
Magnus Toftdal <duckhero@gmail.com>
2019-02-28 13:24:11 +0100

    Removed obsolete property

--- src/Xena.Contracts/Domain/PartnerPostPaymentDto.cs
+++ src/Xena.Contracts/Domain/PartnerPostPaymentDto.cs
@@ -2,9 +2,9 @@ namespace Xena.Contracts.Domain
 {
     public class PartnerPostPaymentDto : FiscalDto,IHasIdDto
     {
+        public long? Id { get; set; }
         public PartnerPostDto PartnerPost { get; set; }
         public decimal AmountPaid { get; set; }
-        public long? Id { get; set; }
         public long PaymentExportedId { get; set; }
     }
 }
