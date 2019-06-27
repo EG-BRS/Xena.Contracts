4606ba4f1f1ac163c2744d45df2ea0d9ae9a17e4
Magnus Toftdal <duckhero@gmail.com>
2019-06-27 14:20:24 +0200

    #JIRA: brs-2063 | Clean up of Data

--- src/Xena.Contracts/Helpers/VatReconciliationTotalData.cs
+++ src/Xena.Contracts/Helpers/VatReconciliationTotalData.cs
@@ -2,13 +2,17 @@ namespace Xena.Contracts.Helpers
 {
     public class VatReconciliationTotalData
     {
+        private decimal? _differenceAmount;
         public long? VatId { get; set; }
         public string VatDescription { get; set; }
         public string VatAbbreviation { get; set; }
         public decimal Basis { get; set; }
         public decimal VatAmount { get; set; }
         public decimal ExpectedVatAmount { get; set; }
-        public decimal DeductableAmount { get; set; }
-        public decimal DeductablePercentage { get; set; }
+        public decimal DifferenceAmount
+        {
+            get => _differenceAmount ?? VatAmount - ExpectedVatAmount;
+            set => _differenceAmount = value;
+        }
     }
 }
