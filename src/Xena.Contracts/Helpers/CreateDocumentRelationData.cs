c802a05d05b4dde8c6884ad2fe64e9d9ed0121b2
Jacek Rachwał <xxggb@eg.dk>
2019-10-15 17:39:57 +0200

    #Jira BRS-20 - Add pseudo folders to Xena - refactor - namespace Xena.Domain.DocumentRelated and Xena.Web.Api.DocumentRelated + first version of migration script

--- src/Xena.Contracts/Helpers/CreateDocumentRelationData.cs
+++ src/Xena.Contracts/Helpers/CreateDocumentRelationData.cs
@@ -3,6 +3,7 @@ namespace Xena.Contracts.Helpers
     public class CreateDocumentRelationData
     {
         public long DocumentId { get; set; }
+        public long? DocumentFolderId { get; set; }
         public long EntityId { get; set; }
         public string ChosenEntity { get; set; }
         public bool NewDocument { get; set; }
