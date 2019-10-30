5ac6d406c8864b1f950fb9607b901fc351f51099
Morten Flyger <morten.flyger@gmail.com>
2019-10-30 10:55:30 +0100

    Adds InvoiceRecipientData

---
+++ src/Xena.Contracts/Helpers/InvoiceRecipientData.cs
@@ -0,0 +1,8 @@
+﻿namespace Xena.Contracts.Helpers
+{
+    public class InvoiceRecipientData
+    {
+        public string RecipientAddressType { get; set; }
+        public string RecipientAddress { get; set; }
+    }
+}
