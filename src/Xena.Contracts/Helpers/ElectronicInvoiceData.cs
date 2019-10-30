3d7e629583ca26cb6be2051607b1458309629488
Morten Flyger <morten.flyger@gmail.com>
2019-10-30 12:33:58 +0100

    Adds InvoiceRecipientData Endpoint and PartyIdentification to ElectronicInvoiceData

--- src/Xena.Contracts/Helpers/ElectronicInvoiceData.cs
+++ src/Xena.Contracts/Helpers/ElectronicInvoiceData.cs
@@ -1,9 +1,13 @@
+using System;
+
 namespace Xena.Contracts.Helpers
 {
     public class ElectronicInvoiceData
     {
         public long OrderInvoiceTransactionId { get; set; }
+        [Obsolete("Use Endpoint instead")]
         public string RecipientAddressType { get; set; }
+        [Obsolete("Use Endpoint instead")]
         public string RecipientAddress { get; set; }
         public string PartnerName { get; set; }
         public long PartnerId { get; set; }
@@ -11,5 +15,7 @@ namespace Xena.Contracts.Helpers
         public int InvoiceNumber { get; set; }
         public string GLNNumber { get; set; }
         public string OrgNumber { get; set; }
+        public InvoiceRecipientData Endpoint { get; set; }
+        public InvoiceRecipientData PartyIdentification { get; set; }
     }
 }
