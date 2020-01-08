ccada997faea2be8156b28392fec54c3684e27c0
awsm <xxdld@eg.dk>
2020-01-08 17:19:49 +0100

    #Jira BRS-3530 | GetCanBookkeep optimization

---
+++ src/Xena.Contracts/Domain/StockCountDraftProblemDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Xena.Contracts.Domain
+{
+    public class StockCountDraftProblemDto : FiscalDto
+    {
+        public long Id { get; set; }
+        public long StockCountId { get; set; }
+        public decimal? Sum { get; set; }
+        public int Count { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal QuantityReserved { get; set; }
+        public string ArticleNumber { get; set; }
+        public string LocationAbbreviation { get; set; }
+        public string ArticleVariantAbbreviation { get; set; }
+    }
+}
